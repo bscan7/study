876da70cf3649e7a14e20974af28f9db25aba1a5
bscan7 <37958991+bscan7@users.noreply.github.com>
2018-06-29 16:09:02 +0800

    000000000000
    000000000000000000000000000000000000

--- CleanVCproj.bat
+++ CleanVCproj.bat
@@ -1,5 +1,5 @@
 @echo off  
-set TMPP=*.~* *.aps *.bak *.dcu *.ddp *.old *.plg *.user *.idb *.ncp *.obj *.pch *.sbr *.tmp *.pdb *.bsc *.ilk *.res *.ncb *.opt *.suo *.dep *.log .git
+set TMPP=*.~* *.aps *.bak *.dcu *.ddp *.old *.plg *.user *.idb *.ncp *.obj *.pch *.sbr *.tmp *.pdb *.bsc *.ilk *.res *.ncb *.opt *.suo *.dep *.log *.sdf *.db *.ipch
 echo ----------------------------------------------------  
 echo By Bscan
 echo 即将删除目录：%cd% 中，所有以下类型的文件:  
